4e6fc6bfcfd4b375e87bc3fcd116c23286b2208b
Rizal Fadlullah <izzal@MacBook-Air-Rizal.local>
2024-07-19 02:53:12 +0700

    feat: Adding feature to show transaction

---
+++ lib/repository/transaction_repository.dart
@@ -0,0 +1,80 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:finance_management/model/transaction.dart' as model;
+import 'package:firebase_auth/firebase_auth.dart';
+
+class TransactionRepository {
+  static final TransactionRepository _instance =
+      TransactionRepository._internal();
+  static final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  static final FirebaseAuth auth = FirebaseAuth.instance;
+
+  factory TransactionRepository() {
+    return _instance;
+  }
+
+  TransactionRepository._internal();
+
+  Future<bool> add(model.Transaction transaction) async {
+    try {
+      String uid = auth.currentUser!.uid;
+      await firestore
+          .collection('users')
+          .doc(uid)
+          .collection('transactions')
+          .add(
+            transaction.toJson(),
+          );
+      return true;
+    } catch (e) {
+      log(e.toString());
+      return false;
+    }
+  }
+
+  Stream<List<model.Transaction>> getAllTransactions() {
+    String uid = auth.currentUser!.uid;
+    return firestore
+        .collection('users')
+        .doc(uid)
+        .collection('transactions')
+        .orderBy('date', descending: true)
+        .snapshots()
+        .map((snapshot) => snapshot.docs
+            .map((doc) => model.Transaction.fromJson(doc.data()))
+            .toList());
+  }
+
+  Future<bool> delete(String id) async {
+    try {
+      String uid = auth.currentUser!.uid;
+      await firestore
+          .collection('users')
+          .doc(uid)
+          .collection('transactions')
+          .doc(id)
+          .delete();
+      return true;
+    } catch (e) {
+      log(e.toString());
+      return false;
+    }
+  }
+
+  Future<bool> update(String id, model.Transaction transaction) async {
+    try {
+      String uid = auth.currentUser!.uid;
+      await firestore
+          .collection('users')
+          .doc(uid)
+          .collection('transactions')
+          .doc(id)
+          .update(transaction.toJson());
+      return true;
+    } catch (e) {
+      log(e.toString());
+      return false;
+    }
+  }
+}
--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,25 +1,163 @@
+import 'package:finance_management/model/transaction.dart';
+import 'package:finance_management/repository/transaction_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:finance_management/widget/transaction_item.dart';
+import 'package:intl/intl.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key, required this.accountInfo}) : super(key: key);
 
+  final User? accountInfo;
+
+  @override
+  HomeViewState createState() => HomeViewState();
+}
+
+class HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Finance Management App'),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: AppBar(
+            title: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(
+                      18), // Penyesuaian sedikit untuk memotong foto dengan baik
+                  child: Image.network(
+                    'https://i.pravatar.cc/50',
+                    fit: BoxFit.cover,
+                    width: 50,
+                    height: 50,
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Hi,',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(widget.accountInfo?.displayName ?? '',
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ],
+                )
+              ],
+            ), // Update title to display account info
+            actions: [
+              IconButton(
+                icon: const Icon(
+                  Icons.logout,
+                  size: 32,
+                ),
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+              ),
+            ],
+          ),
+        ),
       ),
-      body: const Column(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Tindakan yang diambil ketika FAB ditekan
+          print('Floating Action Button pressed');
+        },
+        tooltip: 'Increment',
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Home'),
-          ElevatedButton(onPressed: _logout, child: Text('Logout')),
+          Container(
+            width: double.infinity,
+            height: 200,
+            margin: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              color: Colors.lightBlue,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Saldo Anda saat ini',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'Rp 15.000.000',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: StreamBuilder<List<Transaction>>(
+                stream: TransactionRepository().getAllTransactions(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else {
+                    List<Transaction> transactions = snapshot.data!;
+                    return ListView.builder(
+                      itemCount: transactions.length,
+                      itemBuilder: (context, index) {
+                        int amount = transactions[index].amount;
+                        DateTime date = transactions[index].date;
+                        return TransactionItem(
+                            category: transactions[index].category,
+                            title: transactions[index].name,
+                            date: formatDate(date),
+                            amount: formatCurrency(amount));
+                      },
+                    );
+                  }
+                }),
+          ),
         ],
       ),
     );
   }
 
-  static void _logout() {
-    FirebaseAuth.instance.signOut();
+  String formatCurrency(int amount) {
+  final NumberFormat currencyFormatter = NumberFormat.currency(
+    locale: 'id_ID',
+    symbol: 'Rp',
+    decimalDigits: 0,
+  );
+
+  return currencyFormatter.format(amount);
+}
+
+String formatDate(DateTime dateTime) {
+    // Format tanggal ke format "19 Juli 2024"
+    final DateFormat dateFormatter = DateFormat('d MMMM yyyy', 'id_ID');
+    return dateFormatter.format(dateTime);
   }
 }
